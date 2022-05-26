955cbd65ec193176333926b0e2f495ba5a2bee78
Ahmed M. Taha <ahmedmuhammed181298@gmail.com>
2022-05-27 01:48:00 +0200

    Time: 84 ms (29.45%), Space: 15.6 MB (37.07%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -22,11 +22,11 @@ private:
         
         return HashValue({dy/g, dx/g});
     }
-    vector<unordered_map<ull, int>> pointSlopeData;
+    vector<map<ull, int>> pointSlopeData;
     
 public:
     int maxPoints(vector<vector<int>>& points) {
-        pointSlopeData = vector<unordered_map<ull, int>>(points.size());
+        pointSlopeData = vector<map<ull, int>>(points.size());
         
         int maxP = 0;
         for(int i = 0; i < points.size(); ++i) {
