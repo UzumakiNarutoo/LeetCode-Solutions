7f1cfc20b96c92a293c7cf9f70e09ba4cfa30b19
Ahmed M. Taha <ahmedmuhammed181298@gmail.com>
2022-05-27 01:47:31 +0200

    Time: 60 ms (44.31%), Space: 16.4 MB (35.95%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -0,0 +1,40 @@
+/// The points lie on the same line iff any pair of them have the same slope
+/// slope = dy / dx
+
+typedef unsigned long long ull;
+
+class Solution {
+private:
+    ull HashValue(const vector<int>& slope) {
+        int dy = slope[0], dx = slope[1];
+        
+        bool dySign = dy < 0, dxSign = dx < 0;
+        
+        return ((abs(dy) * 100000ll + abs(dx)) * 10ll + dySign) * 10ll + dxSign; 
+    }
+    
+    ull slope(const vector<int>& point1, const vector<int>& point2) {
+        int x1 = point1[0], y1 = point1[1];
+        int x2 = point2[0], y2 = point2[1];
+        
+        int dy = y1 - y2, dx = x1 - x2;
+        int g = __gcd(dy, dx);
+        
+        return HashValue({dy/g, dx/g});
+    }
+    vector<unordered_map<ull, int>> pointSlopeData;
+    
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        pointSlopeData = vector<unordered_map<ull, int>>(points.size());
+        
+        int maxP = 0;
+        for(int i = 0; i < points.size(); ++i) {
+            for(int j = i+1; j < points.size(); ++j) {
+                maxP = max(maxP, ++pointSlopeData[i][slope(points[i], points[j])]);
+            }
+        }
+        
+        return maxP + 1;
+    }
+};
